1a08e91bc746da0efd0a4de382a1222a412b96bb
Andrei Danishevskii <a@uds.systems>
2019-08-02 14:46:45 +0300

    fixed Notes.js page

--- Core3.WebUI/ClientApp/src/components/Notes.js
+++ Core3.WebUI/ClientApp/src/components/Notes.js
@@ -3,13 +3,12 @@ import { Container, Row, Col } from 'reactstrap';
 import Note from './Note'
 import NoteInput from './NoteInput'
 import { getNotes, createNote } from './Fetch'
-import ErrorPage from './ErrorPage'
 import '../styles/styles.css'
 
 export default class Notes extends Component {
     constructor (props) {
         super(props);
-        this.state = { notes: [], loading: true, err: false};
+        this.state = { notes: [], loading: true};
 
         this.last = React.createRef();
     }
@@ -34,53 +33,42 @@ export default class Notes extends Component {
     }
 
     fetchNotes = async () => {
-        const {obj, status} = await getNotes();
-        if (status === 200)
-            this.setState({notes: obj, loading: false})
-        if (status === 500)
-            this.setState({err: true})
+        const notes = await getNotes();
+        this.setState({notes: notes, loading: false});
     }
 
     async componentDidMount() {
         await this.fetchNotes();
-        if (!this.state.err)
-            this.last.current.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            })
+        this.last.current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
     }
 
     componentDidUpdate() {
-        if (!this.state.err)
-            this.last.current.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            })
+        this.last.current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
     }
 
     render(){
-        let page = <ErrorPage/>
-        if (!this.state.err) {
-            const contents = this.state.loading
+        const contents = this.state.loading
             ? <p><em>Loading...</em></p>
             : this.renderNotes(this.state.notes)
 
-            page = 
-                <Container ref={this.cont}>  
-                    <Row>   
-                        <Col className="note-container">
-                            {contents}
-                            <div ref={this.last}></div>
-                        </Col>
-                    </Row>
-                    <Row>                    
-                        <NoteInput create={this.createNew}/>                    
-                    </Row>
-                </Container>
-        }
-
         return (      
-            page
+            <Container ref={this.cont}>  
+                <Row>   
+                    <Col className="note-container">
+                        {contents}
+                        <div ref={this.last}></div>
+                    </Col>
+                </Row>
+                <Row>                    
+                    <NoteInput create={this.createNew}/>                    
+                </Row>
+            </Container>
         );
     }
 }
